e4e24bbb0e46f57ddb5a92d0f50663c96ecaa109
h-takauma <h-takauma@i-enter.co.jp>
2019-06-19 13:21:27 +0900

    初回コミット

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-container>
+    <v-layout
+      text-xs-center
+      wrap
+    >
+      <v-flex mb-4>
+        <h1 class="display-2 font-weight-bold mb-3">
+          Welcome to about Page
+        </h1>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
