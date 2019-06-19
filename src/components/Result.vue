f821d1abc190834c9e7d31ed5a13dcdaca7f755d
h-takauma <h-takauma@i-enter.co.jp>
2019-06-19 16:43:47 +0900

    結果画面追加

---
+++ src/components/Result.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-container>
+    <v-layout
+      text-xs-center
+      wrap
+    >
+      <v-flex mb-4>
+        <h1 class="display-3 font-weight-bold mb-3">
+          【BMI測定結果】
+        </h1>
+      </v-flex>
+
+      <v-flex
+        xs12
+        mb-5
+      >
+        <h3 class="display-2 font-weight-bold mb-3">
+        {{status}}
+        </h3>
+      </v-flex>
+
+      <v-flex
+        xs12
+        mb-5
+      >
+        <v-btn large color="success" @click="bmiStart">もう一度やる</v-btn>
+      </v-flex>
+
+    </v-layout>
+  </v-container>
+</template>
+
+<style>
+</style>
+<script>
+export default {
+  data () {
+    return {
+      status: ''
+    }
+  },
+  created () {
+    var me = this
+    const callbacks = {
+      onUpdate(state) {
+        if ('bmi' in state) {
+          me.status = state.bmi
+        }
+      },
+    }
+    assistantCanvas.ready(callbacks)
+  },
+  methods: {
+    bmiStart () {
+      assistantCanvas.sendTextQuery('bmi');
+    }
+  }
+}
+</script>
