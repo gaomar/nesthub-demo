bd2692b3684eda53d2d47a8ec04c4fc84cdc11fe
h-takauma <h-takauma@i-enter.co.jp>
2019-06-20 10:33:47 +0900

    てすと

--- src/lambda/bmi.js
+++ src/lambda/bmi.js
@@ -1,6 +1,27 @@
-exports.handler = function(event, context, callback) {
-    callback(null, {
-      statusCode: 200,
-      body: "Hello, World"
-    });
-};
+const functions = require('firebase-functions');
+const {dialogflow, ImmersiveResponse} = require('actions-on-google');
+
+const app = dialogflow({debug: true});
+
+app.intent('welcome', (conv) => {
+  conv.ask('あなたのBMIを測定します。測定開始ボタンを押してください。');
+  conv.ask(new ImmersiveResponse({
+    url: `https://gaomar-nesthub-demo.netlify.com/`,
+    suppress: true
+  }));
+});
+
+app.intent('bmi', (conv, {height, weight}) => {
+  const bmiVal = (parseFloat(weight) / (parseFloat(height)/100 * parseFloat(height)/100)).toFixed(1);
+  var speechText = `あなたのBMIは${bmiVal}です。`;
+  conv.ask(speechText);
+  conv.ask(new ImmersiveResponse({
+    url: `https://gaomar-nesthub-demo.netlify.com/result`,
+    suppress: true,
+    state: {
+      bmi: speechText,
+    },
+  }));
+});
+
+exports.handler = functions.https.onRequest(app);
